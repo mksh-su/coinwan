4bb4c7abbf3e229340aac54ace970185cf20693b
Mikhail Shishov <contact@michaelshishov.com>
2022-09-22 14:44:36 +0200

    var to let

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -16,31 +16,31 @@ const introSwiper = new Swiper(".intro-swiper", {
 });
 
 function Tabs() {
-  var bindAll = function () {
-    var menuElements = document.querySelectorAll("[data-tab]");
-    for (var i = 0; i < menuElements.length; i++) {
+  let bindAll = function () {
+    let menuElements = document.querySelectorAll("[data-tab]");
+    for (let i = 0; i < menuElements.length; i++) {
       menuElements[i].addEventListener("click", change, false);
     }
   };
 
-  var clear = function () {
-    var menuElements = document.querySelectorAll("[data-tab]");
-    for (var i = 0; i < menuElements.length; i++) {
+  let clear = function () {
+    let menuElements = document.querySelectorAll("[data-tab]");
+    for (let i = 0; i < menuElements.length; i++) {
       menuElements[i].classList.remove("active");
-      var id = menuElements[i].getAttribute("data-tab");
+      let id = menuElements[i].getAttribute("data-tab");
       document.getElementById(id).classList.remove("active");
     }
   };
 
-  var change = function (e) {
+  let change = function (e) {
     clear();
     e.target.classList.add("active");
     e.preventDefault();
-    var id = e.currentTarget.getAttribute("data-tab");
+    let id = e.currentTarget.getAttribute("data-tab");
     document.getElementById(id).classList.add("active");
   };
 
   bindAll();
 }
 
-var connectTabs = new Tabs();
+let connectTabs = new Tabs();
