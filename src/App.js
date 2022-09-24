35442de39a78c55ea7cbb8b5c7110f6d2e627755
Mikhail Shishov <contact@michaelshishov.com>
2022-09-24 11:45:40 +0200

    Account dropdown

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 import logo from "./img/logo-white.svg";
 import account from "./img/icons/account.svg";
-import accountInfo from "./img/icons/account.svg";
+import accountInfo from "./img/icons/account-info.svg";
 import accountSecurity from "./img/icons/security.svg";
 import accountVerification from "./img/icons/verification.svg";
 import accountReferrals from "./img/icons/referrals.svg";
@@ -111,14 +111,31 @@ const Home = () => {
     const currency = document.querySelectorAll(".website-settings-currency");
     lang.forEach(function (el) {
       el.addEventListener("click", function () {
-        document.querySelector(".website-language").textContent = el.textContent;
+        document.querySelector(".website-language").textContent =
+          el.textContent;
       });
     });
     currency.forEach(function (el) {
       el.addEventListener("click", function () {
-        document.querySelector(".website-currency").textContent = el.textContent;
+        document.querySelector(".website-currency").textContent =
+          el.textContent;
       });
-    }); 
+    });
+
+    document
+      .querySelector(".account-settings-name-id")
+      .addEventListener("click", function () {
+        // Get the text field
+        var copyText = document.querySelector(
+          ".account-settings-name-id-number"
+        ).textContent;
+
+        // Copy the text inside the text field
+        navigator.clipboard.writeText(copyText);
+
+        // Alert the copied text
+        console.log("Copied the text: " + copyText);
+      });
   }, []);
   return (
     <div className="wrapper">
@@ -156,13 +173,57 @@ const Home = () => {
                     Get Started
                   </a>
                 </li>
-                <li className="nav-list-item">
+                <li className="nav-list-item account-settings-select">
                   <a href="#">
                     <img src={account} alt="" />
                   </a>
 
                   <div className="account-settings">
-                    
+                    <div className="account-settings-id">
+                      <div className="account-settings-avatar">
+                        <span>as</span>
+                      </div>
+                      <div className="account-settings-name">
+                        <span className="account-settings-name-email">
+                          asd*****@gmail.com
+                        </span>
+                        <span className="account-settings-name-id">
+                          UID:{" "}
+                          <span className="account-settings-name-id-number">
+                            0000000001
+                          </span>
+                        </span>
+                      </div>
+                    </div>
+                    <ul className="account-settings-list">
+                      <li>
+                        <a href="#">
+                          <img src={accountInfo} alt="" /> Account information
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <img src={accountSecurity} alt="" /> Security Settings
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <img src={accountVerification} alt="" /> Verification
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <img src={accountReferrals} alt="" /> Referrals
+                        </a>
+                      </li>
+                    </ul>
+                    <ul className="account-settings-list">
+                      <li>
+                        <a href="#">
+                          <img src={accountLogout} alt="" /> Log Out
+                        </a>
+                      </li>
+                    </ul>
                   </div>
                 </li>
                 <li className="nav-list-item website-settings-select">
